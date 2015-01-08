3c62dba434f23f35ba52ef5d883f8db8be6ea8c0
woodsaj <anthony@monkey.id.au>
2015-01-08 18:50:15 +0800

    move config.js to config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -10,36 +10,36 @@ config.deleteTTL = 60*60*24*90;
 
 config.adminToken = 'jk832sjksf9asdkvnngddfg8sfk';
 
-config.mongoURL = 'mongodb://dbuser:dbpass@192.168.2.17/raintank';
+config.mongoURL = 'mongodb://dbuser:dbpass@mongodb/raintank';
 
-config.siteUrl = "http://192.168.2.14/";
+config.siteUrl = "http://proxy/";
 
 config.defaultRole = '5314801a421408bcac0a6448';
-config.primaryLocation = '5337ed35033bb9203cfc1849';
 
 config.emailFrom = "alerts@raintank.io";
 
 config.graphite_api = {
-	host: '192.168.2.17',
-	port: 8888
+  host: 'graphite-api',
+  port: 8888
 }
 
 config.carbon = {
-	host: '192.168.2.17',
-	port: 2003
+  host: 'influxdb',
+  port: 2003
 }
 
 config.numCPUs = 1;
 
 config.elasticSearch = {
-  host: '192.168.2.17:9200',
+  host: 'elasticsearch:9200',
   log: 'info'
 };
 
 config.queue = {
-	publisherSocketAddr: 'tcp://127.0.0.1:9997',
-	partitions: 10,
-	mgmtUrl: "http://localhost:9999"
+  publisherSocketAddr: 'tcp://broker:9997',
+  consumerSocketAddr: "tcp://broker:9998",
+  partitions: 10,
+  mgmtUrl: "http://broker:9999"
 };
 
 config.port = 4000;
@@ -48,31 +48,31 @@ config.grafana_path = '/opt/grafana/src'; //Dev
 
 /*-------------------------------------------------------*/
 function parseEnv(name, value, cnf) {
-	if (name in cnf) {
-		cnf[name] = value;
-		return;
-	}
-	var parts = name.split('_');
-	var pos = 0
-	var found = false
-	while (!found && pos < parts.length) {
-		pos = pos + 1;
-		var group = parts.slice(0,pos).join('_');
-		if (group in cnf){
-			found = true;
-			parseEnv(parts.slice(pos).join('_'), value, cnf[group]);
-		}
-	}
-	if (!found) {
-		console.log("%s not found in config", name);
-	}
+  if (name in cnf) {
+    cnf[name] = value;
+    return;
+  }
+  var parts = name.split('_');
+  var pos = 0
+  var found = false
+  while (!found && pos < parts.length) {
+    pos = pos + 1;
+    var group = parts.slice(0,pos).join('_');
+    if (group in cnf){
+      found = true;
+      parseEnv(parts.slice(pos).join('_'), value, cnf[group]);
+    }
+  }
+  if (!found) {
+    console.log("%s not found in config", name);
+  }
 }
 
 // overwrite with Environment variables
 for (var key in process.env) {
-	if (key.indexOf('RAINTANK_') == 0) {
-		var name = key.slice(9);
-		parseEnv(name, process.env[key], config);
-	}
+  if (key.indexOf('RAINTANK_') == 0) {
+    var name = key.slice(9);
+    parseEnv(name, process.env[key], config);
+  }
 }
 exports.config = config;
